853df527c3203c5485e7e6b2d19c5f1e6d150a0a
ArturCiepielowski <artciepi@wp.pl>
2019-07-07 21:20:52 +0200

    feat: color change

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,47 @@
+
 "use strict";
 
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+
+
+
+document.getElementById("myButton--js").onclick=function(){
+
+var name=prompt('Cześć .... eee możesz powtórzyć jak masz na mię ?','');
+var age=prompt(`Witaj, ${name} powiedz mi proszę .. masz więcej niż 18 lat .. prawda ? `,'');
+
+if(age>=18){
+
+  alert('Super to idziemy na piwo!');
+
+} else if(age<18){
+
+    alert('No nic to niestety soczek dla ciebie');
+} else {
+
+    alert('Miałeś wpisać swoj wiek..... TYPIE!');
+}
+
+}
+
+
+const magicWord = document.querySelector('.paragraph__test--js');
+
+console.log(magicWord.innerHTML);
+
+magicWord.innerHTML='Magiczne słowo stworzone przez JavaScript'
+
+
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
+
+
+
---
+++ src/js/secound.js
@@ -0,0 +1,21 @@
+
+"use strict";
+
+import moment from 'moment';
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+
+
+
+
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
+
+
+
